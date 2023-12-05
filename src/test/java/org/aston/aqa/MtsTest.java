f8e4f528836f7125ce145aa36c1a117e943a3c5b
GlebPishchalo <56067349+GlebPishchalo@users.noreply.github.com>
2023-12-05 22:37:28 +0300

    feat:successful les_13

---
+++ src/test/java/org/aston/aqa/MtsTest.java
@@ -0,0 +1,84 @@
+package org.aston.aqa;
+
+import static org.junit.Assert.assertTrue;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.function.Function;
+import static org.junit.Assert.assertEquals;
+
+
+public class MtsTest
+{
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://mts.by/");
+    }
+
+    @Test
+    public void testBlockTitle() {
+        WebElement blockTitle = driver.findElement(By.xpath("//div[contains(@class, 'pay__wrapper')]/h2"));
+        assertEquals("Онлайн пополнение\n" +
+                "без комиссии", blockTitle.getText());
+    }
+
+    @Test
+    public void testPaymentSystemLogos() {
+        java.util.List<WebElement> logos = driver.findElements(By.xpath("//div[@class='pay__partners']//img"));
+        assertTrue(logos.size() > 0);
+    }
+
+    @Test
+    public void testServiceDetailsLink() {
+        //у меня возникли проблемы с этим методом, я использовал ExpectedConditions.visibilityOfElementLocated но оно не видело 'cookie show'
+        //не по названию класса не по xPath,  я нашел в интернете js executor, который игнорирует это. Не знаю правильно ли это?
+        WebElement detailsLink = driver.findElement(By.xpath("//a[text()='Подробнее о сервисе']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", detailsLink);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.urlContains("help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
+    }
+
+    @Test
+    public void testContinueButton() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement cookiePopup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cookie")));
+        WebElement closeButton = cookiePopup.findElement(By.className("cookie__close"));
+        closeButton.click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("cookie")));
+        WebElement optionElement = driver.findElement(By.xpath("//option[@value='Услуги связи']"));
+        optionElement.click();
+        WebDriverWait formWait = new WebDriverWait(driver, 10);
+        formWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@id='pay-connection']")));
+        WebElement phoneInput = driver.findElement(By.xpath("//input[@id='connection-phone']"));
+        phoneInput.sendKeys("297777777");
+        WebElement sumInput = driver.findElement(By.xpath("//input[@id='connection-sum']"));
+        sumInput.sendKeys("100");
+        WebElement emailInput = driver.findElement(By.xpath("//input[@id='connection-email']"));
+        emailInput.sendKeys("test@example.com");
+        WebElement continueButton = driver.findElement(By.xpath("//form[@id='pay-connection']//button[@type='submit']"));
+        continueButton.click();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
