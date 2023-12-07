50bb56e8a2e973f54dd91746d522171040e8e5c1
GlebPishchalo <56067349+GlebPishchalo@users.noreply.github.com>
2023-12-07 20:46:59 +0300

    feat:sucessful lesson 14

--- src/test/java/org/aston/aqa/MtsTest.java
+++ src/test/java/org/aston/aqa/MtsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -12,13 +13,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.function.Function;
+
+
 import static org.junit.Assert.assertEquals;
 
 
-public class MtsTest
-{
+public class MtsTest {
     private WebDriver driver;
 
     @Before
@@ -73,6 +75,75 @@ public class MtsTest
         emailInput.sendKeys("test@example.com");
         WebElement continueButton = driver.findElement(By.xpath("//form[@id='pay-connection']//button[@type='submit']"));
         continueButton.click();
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.className("bepaid-iframe")));
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("cc-number")));
+        WebElement amountLabel = driver.findElement(By.xpath("//p[@class='header__payment-amount']"));
+        String displayedAmount = amountLabel.getText();
+        assertEquals("100.00 BYN", displayedAmount);
+
+        WebElement paymentInfo = driver.findElement(By.xpath("//p[@class='header__payment-info']"));
+        String displayedPaymentInfo = paymentInfo.getText();
+        assertTrue(displayedPaymentInfo.contains("Оплата: Услуги связи Номер:375297777777"));
+        WebElement cardNumber = driver.findElement(By.xpath("//label[@class='ng-tns-c47-1 ng-star-inserted']"));
+        String displayedcardNumber = cardNumber.getText();
+        assertTrue(displayedcardNumber.contains("Номер карты"));
+        WebElement dateOfCard = driver.findElement(By.xpath("//label[@class='ng-tns-c47-4 ng-star-inserted']"));
+        String displayedDateOfCard = dateOfCard.getText();
+        assertTrue(displayedDateOfCard.contains("Срок действия"));
+        WebElement cvcNumber = driver.findElement(By.xpath("//label[@class='ng-tns-c47-5 ng-star-inserted']"));
+        String displayedcvcNumber = cvcNumber.getText();
+        assertTrue(displayedcvcNumber.contains("CVC"));
+        WebElement nameCard = driver.findElement(By.xpath("//label[@class='ng-tns-c47-3 ng-star-inserted']"));
+        String displayednameCard = nameCard.getText();
+        assertTrue(displayednameCard.contains("Имя держателя (как на карте)"));
+
+        java.util.List<WebElement> logos = driver.findElements(By.xpath("//div[@class='cards-brands cards-brands__container ng-tns-c53-0 ng-trigger ng-trigger-brandsState ng-star-inserted']//img"));
+        assertTrue(logos.size() > 0);
+
+
+    }
+
+    @Test
+    public void testDomashniyInternet() {
+        WebElement selectElement = driver.findElement(By.id("pay"));
+        Select select = new Select(selectElement);
+        select.selectByValue("Домашний интернет");
+        WebElement internetPhoneInput = driver.findElement(By.id("internet-phone"));
+        WebElement internetSumInput = driver.findElement(By.id("internet-sum"));
+        WebElement internetEmailInput = driver.findElement(By.id("internet-email"));
+        Assert.assertEquals(internetPhoneInput.getAttribute("placeholder"), "Номер абонента");
+        Assert.assertEquals(internetSumInput.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(internetEmailInput.getAttribute("placeholder"), "E-mail для отправки чека");
+    }
+
+    @Test
+    public void testRassrochka() {
+        WebElement selectElement = driver.findElement(By.id("pay"));
+        Select select = new Select(selectElement);
+        select.selectByValue("Рассрочка");
+        WebElement internetPhoneInput = driver.findElement(By.id("score-instalment"));
+        WebElement internetSumInput = driver.findElement(By.id("instalment-sum"));
+        WebElement internetEmailInput = driver.findElement(By.id("instalment-email"));
+        Assert.assertEquals(internetPhoneInput.getAttribute("placeholder"), "Номер счета на 44");
+        Assert.assertEquals(internetSumInput.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(internetEmailInput.getAttribute("placeholder"), "E-mail для отправки чека");
+
+
+    }
+
+    @Test
+    public void testZadolzhenost() {
+        WebElement selectElement = driver.findElement(By.id("pay"));
+        Select select = new Select(selectElement);
+        select.selectByValue("Задолженность");
+        WebElement internetPhoneInput = driver.findElement(By.id("score-arrears"));
+        WebElement internetSumInput = driver.findElement(By.id("arrears-sum"));
+        WebElement internetEmailInput = driver.findElement(By.id("arrears-email"));
+        Assert.assertEquals(internetPhoneInput.getAttribute("placeholder"), "Номер счета на 2073");
+        Assert.assertEquals(internetSumInput.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(internetEmailInput.getAttribute("placeholder"), "E-mail для отправки чека");
     }
 
     @After
